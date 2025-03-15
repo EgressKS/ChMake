e1b0e5c8429b64d94219cccbe6a1f95835025cd8
dev-KS-155 <devcreator155@gmail.com>
2025-03-15 23:35:32 +0530

    deployment fix 10

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL:  "http://localhost:5001/api" ,
-  baseURL: "https://ch-make-backend.vercel.app/api",
+  baseURL:  "http://localhost:5001/api" ,
+  // baseURL: "https://ch-make-backend.vercel.app/api",
   withCredentials: true,
 });
