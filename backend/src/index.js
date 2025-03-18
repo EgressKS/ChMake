7e419f6da944e7de87fcee2b5126236a2eca61c3
dev-KS-155 <devcreator155@gmail.com>
2025-03-19 02:09:27 +0530

    Test DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
